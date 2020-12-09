93d7b057c44bdb0bdee546b845d5560373237752
Tom Counsell <other@tomcounsell.com>
2020-12-09 19:47:48 +0700

    merge json view into html view

--- apps/finance/urls.py
+++ apps/finance/urls.py
@@ -1,15 +1,14 @@
 from django.urls import path
 from django.views.generic import TemplateView
 
-from apps.finance.views import ticker, data
+from apps.finance.views import ticker
 
 app_name = "finance"
 
 urlpatterns = [
 
     # THINGS THAT NEED A UI
+    path('ticker/<str:ticker_symbol>.json', ticker.Ticker.as_view(), kwargs={'return_json': True}, name='ticker.json'),
     path('ticker/<str:ticker_symbol>', ticker.Ticker.as_view(), name='ticker'),
-
-    path('data/<str:ticker_symbol>.json', data.Ticker.as_view(), name='data'),
 
 ]
