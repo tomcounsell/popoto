72422622ee52df6584d1b9ff055537153943e06a
Tom Counsell <other@tomcounsell.com>
2020-12-16 08:48:10 +0000

    standard techan js data vars

--- apps/finance/static/js/candlesticks.js
+++ apps/finance/static/js/candlesticks.js
@@ -1,5 +1,5 @@
 
-var data = ohlc_values;
+var data = CHART_VALUES;
 
 var margin = {top: 20, right: 20, bottom: 30, left: 50},
         width = 960 - margin.left - margin.right,
--- apps/finance/static/js/line.js
+++ apps/finance/static/js/line.js
@@ -1,5 +1,5 @@
 
-var data = chart_values;
+var data = CHART_VALUES;
 
 var margin = {top: 20, right: 20, bottom: 30, left: 50},
         width = 960 - margin.left - margin.right,
@@ -22,47 +22,48 @@ var xAxis = d3.axisBottom(x);
 
 var yAxis = d3.axisLeft(y);
 
-var svg = d3.select("body").append("svg")
+var svg = d3.select("svg.line")
         .attr("width", width + margin.left + margin.right)
         .attr("height", height + margin.top + margin.bottom)
         .append("g")
         .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-d3.csv("data.csv", function(error, data) {
-    var accessor = close.accessor();
 
-    data = data.slice(0, 200).map(function(d) {
-        return {
-            date: parseDate(d.Date),
-            open: +d.Open,
-            high: +d.High,
-            low: +d.Low,
-            close: +d.Close,
-            volume: +d.Volume
-        };
-    }).sort(function(a, b) { return d3.ascending(accessor.d(a), accessor.d(b)); });
+var accessor = close.accessor();
 
-    svg.append("g")
-            .attr("class", "close");
+data = data.slice(0, 200).map(function(d) {
+            return {
+                date: parseDate(d.Date),
+                open: +d.Close,
+                high: +d.Close,
+                low: +d.Close,
+                close: +d.Close,
+                volume: 0
+            };
 
-    svg.append("g")
-            .attr("class", "x axis")
-            .attr("transform", "translate(0," + height + ")");
+}).sort(function(a, b) { return d3.ascending(accessor.d(a), accessor.d(b)); });
 
-    svg.append("g")
-            .attr("class", "y axis")
-            .append("text")
-            .attr("transform", "rotate(-90)")
-            .attr("y", 6)
-            .attr("dy", ".71em")
-            .style("text-anchor", "end")
-            .text("Price ($)");
+svg.append("g")
+        .attr("class", "close");
 
-    // Data to display initially
-    draw(data.slice(0, data.length-20));
-    // Only want this button to be active if the data has loaded
-    d3.select("button").on("click", function() { draw(data); }).style("display", "inline");
-});
+svg.append("g")
+        .attr("class", "x axis")
+        .attr("transform", "translate(0," + height + ")");
+
+svg.append("g")
+        .attr("class", "y axis")
+        .append("text")
+        .attr("transform", "rotate(-90)")
+        .attr("y", 6)
+        .attr("dy", ".71em")
+        .style("text-anchor", "end")
+        .text("Price ($)");
+
+// Data to display initially
+draw(data.slice(0, data.length));
+// Only want this button to be active if the data has loaded
+d3.select("button").on("click", function() { draw(data); }).style("display", "inline");
+
 
 function draw(data) {
     x.domain(data.map(close.accessor().d));
