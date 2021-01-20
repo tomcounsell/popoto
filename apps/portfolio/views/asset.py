c724fbc5d808c4e5b9c6ce39ed7da9b3c3e5c895
Tom Counsell <other@tomcounsell.com>
2021-01-20 12:22:22 +0700

    edit asset view and template

--- apps/portfolio/views/asset.py
+++ apps/portfolio/views/asset.py
@@ -1,10 +1,10 @@
 from django.forms import ModelForm
-from django.http import JsonResponse, HttpResponse
-from django.shortcuts import get_object_or_404, render
+from django.http import JsonResponse, HttpResponse, HttpResponseRedirect
+from django.shortcuts import get_object_or_404, render, redirect
 from django.views import View
 from rest_framework import status
 
-from apps.portfolio.models import Allocation, Asset
+from apps.portfolio.models import Allocation, Asset, Portfolio
 
 
 class AssetForm(ModelForm):
@@ -15,29 +15,48 @@ class AssetForm(ModelForm):
 class AllocationForm(ModelForm):
     class Meta:
         model = Allocation
-        fields = ['quantity_offline', ]
+        fields = ['quantity_offline', 'portfolio',]
 
 
 class AssetView(View):
     def dispatch(self, request, asset_symbol="", *args, **kwargs):
-        self.asset = Asset.objects.filter(asset__symbol=asset_symbol).first()
-        if asset_symbol and not self.allocation:
+        self.asset = Asset.objects.filter(symbol=asset_symbol).first()
+        if asset_symbol and not self.asset:
             return HttpResponse(status=status.HTTP_404_NOT_FOUND)
-        self.context = {
-            "allocation": self.allocation,
-        }
         return super().dispatch(request, *args, **kwargs)
 
     def get(self, request, *args, **kwargs):
-        self.context['allocation_form'] = AssetForm(instance=self.allocation)
-        return render(request, 'portfolio/allocation_edit.html', self.context)
+        self.asset_form = AssetForm(instance=self.asset)
+        self.context = {
+            "asset": self.asset,
+            "asset_form": self.asset_form,
+        }
+        return render(request, 'asset/edit_asset.html', self.context)
 
     def post(self, request, *args, **kwargs):
-        asset_form = AssetForm(request.POST, instance=self.allocation.asset)
-        allocation_form = AssetForm(request.POST, instance=self.allocation)
+        asset_form = AssetForm(request.POST, instance=self.asset)
         if asset_form.is_valid():
-            # add asset to allocation form
-            pass
-        if allocation_form.is_valid():
-            self.allocation = allocation_form.save()
-        return JsonResponse(self.allocation.__dict__)
+            self.asset = asset_form.save()
+
+        portfolio = Portfolio.objects.filter(user=request.user, id=request.POST.get('portfolio')).first()
+        if portfolio:
+            allocation = Allocation.objects.get_or_create(portfolio=portfolio, asset=self.asset)
+            allocation.quantity_offline = request.POST.get('quantity_offline', 0)
+            allocation.save()
+
+        if "portfolio" in request.META.get('HTTP_REFERER'):
+            return redirect(request.get_full_path())
+        else:
+            return redirect('portfolio:assets')
+
+
+class AssetsView(View):
+    def dispatch(self, request, asset_symbol="", *args, **kwargs):
+        return super().dispatch(request, *args, **kwargs)
+
+    def get(self, request, *args, **kwargs):
+        self.context = {
+            "assets": Asset.objects.all(),
+            "asset_form": AssetForm(),
+        }
+        return render(request, 'asset/assets.html', self.context)
