541e192dbee24339acbf7b33d6edb2b1ecc7a4b4
Tom Counsell <contact@tomcounsell.com>
2021-10-20 17:32:38 +0700

    improve publisher from ai app requirements

--- src/popoto/pubsub/publisher.py
+++ src/popoto/pubsub/publisher.py
@@ -13,24 +13,33 @@ class PublisherException(Exception):
 
 
 class Publisher(ABC):
-    channel_name: str = ""
-    publish_data: dict = {}
+    _channel_name: str = ""
+    _publish_data: dict = {}
 
-    def __init__(self, channel_name="", *args, **kwargs):
-        self.channel_name = kwargs.get('channel_name', self.__class__.__name__)
+    def __init__(self, *args, **kwargs):
+        self._channel_name = kwargs.get('channel_name', self.__class__.__name__)
+        super().__init__(*args, **kwargs)
+
+    @property
+    def channel_name(self):
+        return self._channel_name
+
+    @channel_name.setter
+    def channel_name(self, value):
+        self._channel_name = value
 
     def publish(self, data: dict = None, channel_name: str = None, pipeline=None):
         # logger.debug(f"publish to {channel_name}: {publish_data}")
-        channel_name = channel_name or self.channel_name
-        self.publish_data = data or self.publish_data
-        if self.publish_data is None or self.publish_data is {}:
+        channel_name = channel_name or self._channel_name
+        self._publish_data = data or self._publish_data
+        if self._publish_data is None or self._publish_data is {}:
             return None
         elif not channel_name:
             raise PublisherException("missing channel to publish to")
 
         if pipeline:
-            return pipeline.publish(self.channel_name, msgpack.dumps(self.publish_data))
+            return pipeline.publish(self._channel_name, msgpack.dumps(self._publish_data))
         else:
-            subscriber_count = POPOTO_REDIS_DB.publish(channel_name, msgpack.dumps(self.publish_data))
+            subscriber_count = POPOTO_REDIS_DB.publish(channel_name, msgpack.dumps(self._publish_data))
             logger.debug(f"published data to `{channel_name}`, {subscriber_count} subscribers")
             return subscriber_count
