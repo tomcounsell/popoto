da8929ec238f07d06c9e9a8f979578285e30e9aa
Tom Counsell <contact@tomcounsell.com>
2021-11-18 11:30:50 +0700

    sortedset saving and deleting

--- src/popoto/fields/sorted_field.py
+++ src/popoto/fields/sorted_field.py
@@ -1,6 +1,6 @@
-import re
 from decimal import Decimal
 from datetime import date, datetime
+import typing
 import redis
 
 from .field import Field
@@ -13,47 +13,28 @@ class SortedField(Field):
             Toys.query.filter(price__lte=4.99)
             DairyProduct.query.filter(best_before_date__gte=datetime.now())
         Requirements:
-            Must be numeric type (int, float, date, datetime)
+            Must be numeric type (int, float, Decimal, Date, Datetime)
             Null values not allowed. Can set a default.
     """
-    null = False
+    type: type = float
+    null: bool = False
+    default: float = 0
 
     def __init__(self, **kwargs):
         super().__init__()
-        new_kwargs = {  # default
-            'is_sort_key': True,
+        sortedfield_options = {  # default
+            'type': float,
+            'null': False,
+            'default': 0,
         }
-        if kwargs.get('null', None) == True:
+        # set geofield_options, let kwargs override
+        for k, v in sortedfield_options.items():
+            setattr(self, k, kwargs.get(k, v))
+        if self.null is not False:
             from ..models.base import ModelException
-            raise ModelException("sort field cannot be null")
-        new_kwargs.update(kwargs)
-        for k in new_kwargs:
-            setattr(self, k, new_kwargs[k])
-
-    @classmethod
-    def convert_to_numeric(cls, field_type, value):
-        if field_type in [int, float]:
-            return value
-        if field_type is Decimal:
-            return float(value)
-        if field_type is date:
-            return value.toordinal(date)
-        if field_type is datetime:
-            return value.timestamp()
-
-    @classmethod
-    def post_save(cls, model, field_name, numeric_value, pipeline=None):
-        z_add_data = {
-            "key": model.db_key,
-            "name": f'{field_name}:{numeric_value}',
-            "score": numeric_value
-        }
-
-        if isinstance(pipeline, redis.client.Pipeline):
-            pipeline = pipeline.zadd(z_add_data["key"], z_add_data["name"])
-            return pipeline
-        else:
-            return POPOTO_REDIS_DB.zadd(z_add_data["key"], z_add_data["name"])
+            raise ModelException("SortedField cannot be null")
+            # todo: allow null in SortedField. null removes instance from SortedSet
+            # todo: if allow null, how to filter by null value? use extra index set just for nulls?
 
     def get_filter_query_params(self, field_name):
         return super().get_filter_query_params(field_name) + [
@@ -63,7 +44,54 @@ class SortedField(Field):
             f'{field_name}__lt',
             f'{field_name}__lte',
             # f'{field_name}__range',  # todo: like https://docs.djangoproject.com/en/3.2/ref/models/querysets/#range
+            # f'{field_name}__isnull',  # todo: see todo in __init__
         ]
+
+    @classmethod
+    def is_valid(cls, field, value) -> bool:
+        if not super().is_valid(field, value):
+            return False
+        if not isinstance(value, field.type):
+            return False
+        return True
+
+    @classmethod
+    def format_value_pre_save(cls, field_value):
+        if cls.type in [int, float]:
+            return field_value
+        elif cls.type is Decimal:
+            return float(field_value)
+        elif cls.type is date:
+            return field_value.toordinal(date)
+        elif cls.type is datetime:
+            return field_value.timestamp()
+        else:
+            raise ValueError("SortedField received non-numeric value.")
+
+    @classmethod
+    def get_sortedset_db_key(cls, model, field_name):
+        return cls.get_special_use_field_db_key(model, field_name)
+
+    @classmethod
+    def on_save(cls, model: 'Model', field_name: str, field_value: typing.Union[int, float], pipeline=None):
+        sortedset_db_key, sortedset_member, sortedset_score = (
+            cls.get_sortedset_db_key(model, field_name), model.db_key, field_value
+        )
+        if isinstance(pipeline, redis.client.Pipeline):
+            return pipeline.zadd(sortedset_db_key, {sortedset_member: sortedset_score})
+        else:
+            return POPOTO_REDIS_DB.zadd(sortedset_db_key, {sortedset_member: sortedset_score})
+
+    @classmethod
+    def on_delete(cls, model: 'Model', field_name: str, pipeline=None):
+        sortedset_db_key, sortedset_member = (
+            cls.get_sortedset_db_key(model, field_name), model.db_key
+        )
+        if pipeline:
+            return pipeline.zrem(sortedset_db_key, sortedset_member)
+        else:
+            return POPOTO_REDIS_DB.zrem(sortedset_db_key, sortedset_member)
+
 
     @classmethod
     def filter_query(cls, model: 'Model', field_name: str, **query_params) -> set:
