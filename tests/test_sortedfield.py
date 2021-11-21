62f45c6c8933ec5d3d77d8aa524b7dbfa0599edb
Tom Counsell <contact@tomcounsell.com>
2021-11-21 15:10:43 +0700

    example comprehensive test

--- tests/test_sortedfield.py
+++ tests/test_sortedfield.py
@@ -1,11 +1,10 @@
-import sys
-import os
 import datetime
+import os
+import sys
 
 SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
 sys.path.append(os.path.dirname(SCRIPT_DIR))
 
-from src.popoto.redis_db import POPOTO_REDIS_DB
 from src import popoto
 
 
