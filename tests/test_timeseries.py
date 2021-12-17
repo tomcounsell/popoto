a0c1b1b77368cc4812f27df7497e204d80fb97ff
Tom Counsell <contact@tomcounsell.com>
2021-12-17 11:58:32 +0700

    Create test_timeseries.py

---
+++ tests/test_timeseries.py
@@ -0,0 +1,47 @@
+from decimal import Decimal
+import os
+import sys
+from datetime import datetime, timedelta
+from src import popoto
+from src.popoto.redis_db import POPOTO_REDIS_DB
+
+SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
+sys.path.append(os.path.dirname(SCRIPT_DIR))
+
+
+class AssetPrice(popoto.Model):
+    _uid = popoto.KeyField(type=str, null=False)
+    asset_id = popoto.KeyField(type=str, null=False, partition_model=True)
+    price = popoto.Field(type=Decimal)
+    timestamp = popoto.SortedField(type=datetime, null=False)
+
+    def pre_save(self, **kwargs):
+        self._uid = f"{self.asset_id}{self.timestamp.timestamp()}"
+        return super().pre_save(**kwargs)
+
+
+price_history = {
+    datetime(2021, 1, 1): {
+        'BTC': Decimal("29374.15"),
+        'ETH': Decimal("730.37"),
+    },
+    datetime(2021, 1, 2): {
+        'BTC': Decimal("32127.27"),
+        'ETH': Decimal("774.53"),
+    },
+    datetime(2021, 1, 3): {
+        'BTC': Decimal("32782.02"),
+        'ETH': Decimal("975.51"),
+    }
+}
+
+for timestamp in price_history.keys():
+    for asset_id in price_history[timestamp].keys():
+        AssetPrice.create(
+            asset_id=asset_id,
+            price=price_history[timestamp][asset_id],
+            timestamp=timestamp
+        )
+
+query_results = AssetPrice.query.filter(timestamp__gte=datetime(2021, 1, 1), asset_id="BTC")
+assert len(query_results) == 3
