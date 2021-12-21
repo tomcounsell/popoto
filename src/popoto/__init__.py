426d5c94118e85fdab6151212146f24afc30b6b0
Tom Counsell <contact@tomcounsell.com>
2021-12-21 20:47:04 +0700

    fix imports

--- src/popoto/__init__.py
+++ src/popoto/__init__.py
@@ -1,14 +1,13 @@
+from .fields.shortcuts import IntField, KeyField, AutoKeyField, UniqueKeyField, SortedField
 from .fields.field import Field
-from .fields.key_field import KeyField, UniqueKeyField, AutoKeyField
 from .fields.geo_field import GeoField
-from .fields.sorted_field import SortedField
 from .models.base import Model, ModelBase
 from .pubsub.publisher import Publisher
 from .pubsub.subscriber import Subscriber
 
 __all__ = [
     'Field',
-    'KeyField', 'UniqueKeyField', 'AutoKeyField',
+    'KeyField', 'AutoKeyField', 'UniqueKeyField',
     'GeoField',
     'SortedField',
     'Model', 'ModelBase',
