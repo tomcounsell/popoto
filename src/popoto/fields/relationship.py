45156dd52b96d8ad08769fb863ba4224784e4741
Tom Counsell <contact@tomcounsell.com>
2022-01-03 17:55:30 +0700

    relationship saving, loading

--- src/popoto/fields/relationship.py
+++ src/popoto/fields/relationship.py
@@ -2,6 +2,7 @@ import redis
 from .field import Field
 import logging
 
+from ..models.db_key import DB_key
 from ..models.query import QueryException
 from ..redis_db import POPOTO_REDIS_DB
 
@@ -42,31 +43,41 @@ class Relationship(Field):
         ]
 
     @classmethod
-    def on_save(cls, model_instance: 'Model', field_name: str, field_value: str, pipeline=None, **kwargs):
+    def on_save(cls, model_instance: 'Model', field_name: str, field_value: 'Model', pipeline=None, **kwargs):
         from ..models.base import Model
         # on a one-to-many, save the set of many with the related instance
         # add this instance's id to a relationship set based on the related model
         # example: "$RelationshipF:Membership:person:person_db_key"
-        relationship_set_key = f'{cls.get_special_use_field_db_key(model_instance, field_name)}:{field_value.db_key}'
+        relationship_set_db_key = DB_key(
+            cls.get_special_use_field_db_key(model_instance, field_name),
+            field_value.db_key if field_value else "None"
+        )
 
         if pipeline and field_value is None:
-            return pipeline.srem(relationship_set_key, model_instance.db_key)
+            return pipeline.srem(relationship_set_db_key.redis_key, model_instance.db_key.redis_key)
         elif pipeline and isinstance(field_value, Model):
-            return pipeline.sadd(relationship_set_key, model_instance.db_key)
+            return pipeline.sadd(relationship_set_db_key.redis_key, model_instance.db_key.redis_key)
         elif field_value is None:
-            return POPOTO_REDIS_DB.srem(relationship_set_key, model_instance.db_key)
+            return POPOTO_REDIS_DB.srem(relationship_set_db_key.redis_key, model_instance.db_key.redis_key)
         elif isinstance(field_value, Model):
-            return POPOTO_REDIS_DB.sadd(relationship_set_key, model_instance.db_key)
+            return POPOTO_REDIS_DB.sadd(relationship_set_db_key.redis_key, model_instance.db_key.redis_key)
         else:
             return pipeline if pipeline else None
 
     @classmethod
     def on_delete(cls, model_instance: 'Model', field_name: str, field_value, pipeline: redis.client.Pipeline = None, **kwargs):
-        relationship_set_key = f'{cls.get_special_use_field_db_key(model_instance, field_name)}:{field_value.db_key}'
+        # todo: it's possible this instance is not fully loaded or has been changed.
+        #  Need to reload from db before deleting
+        #  Example: on person.friend.delete() the person.friend.friend will have field_value as a keystring
+        #  It will not be a model instance, so this method will fail on field_value.db_key below
+        relationship_set_db_key = DB_key(
+            cls.get_special_use_field_db_key(model_instance, field_name),
+            field_value.db_key if field_value else "None"
+        )
         if pipeline:
-            return pipeline.srem(relationship_set_key, model_instance.db_key)
+            return pipeline.srem(relationship_set_db_key.redis_key, model_instance.db_key.redis_key)
         else:
-            return POPOTO_REDIS_DB.srem(relationship_set_key, model_instance.db_key)
+            return POPOTO_REDIS_DB.srem(relationship_set_db_key.redis_key, model_instance.db_key.redis_key)
 
     @classmethod
     def filter_query(cls, model: 'Model', field_name: str, **query_params) -> set:
@@ -81,20 +92,15 @@ class Relationship(Field):
         keys_lists_to_intersect = list()
         pipeline = POPOTO_REDIS_DB.pipeline()
 
-        def clean_query_value(value: str) -> str:
-            for char in "'?*^[]-/":
-                value = value.replace(char, f"/{char}")
-            return value
-
         for query_param, query_value in query_params.items():
 
             if query_param == f'{field_name}':
                 if not isinstance(query_value, Model):
                     raise QueryException(f"Query filter on Relationship expects model instance. Instead, got {query_value}")
 
-                relationship_set_key = f'{cls.get_special_use_field_db_key(model, field_name)}:{clean_query_value(query_value.db_key)}'
+                relationship_set_db_key = DB_key(cls.get_special_use_field_db_key(model, field_name), query_value.db_key)
                 keys_lists_to_intersect.append(
-                    POPOTO_REDIS_DB.smembers(relationship_set_key)
+                    POPOTO_REDIS_DB.smembers(relationship_set_db_key.redis_key)
                 )
 
             elif query_param.startswith(f'{field_name}__'):
@@ -108,8 +114,11 @@ class Relationship(Field):
                 # note: this will be recursive if references another relationship and so forth
 
                 for relationship_field_value in relationship_field_values:
-                    relationship_set_key = f'{cls.get_special_use_field_db_key(model, field_name)}:{clean_query_value(relationship_field_value.db_key)}'
-                    pipeline.smembers(relationship_set_key)
+                    relationship_set_db_key = DB_key(
+                        cls.get_special_use_field_db_key(model, field_name),
+                        relationship_field_value.db_key
+                    )
+                    pipeline.smembers(relationship_set_db_key.redis_key)
 
         keys_lists_to_intersect += pipeline.execute()
         logger.debug(keys_lists_to_intersect)
