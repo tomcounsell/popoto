ce29017d5cdfb48bc06816bd081adfbf43d466c1
Tom Counsell <contact@tomcounsell.com>
2022-01-03 17:55:55 +0700

    fix encoding for tuples

--- src/popoto/models/encoding.py
+++ src/popoto/models/encoding.py
@@ -1,53 +1,52 @@
-from decimal import Decimal
-import msgpack
 import datetime
-from collections.abc import Iterable
+from collections import namedtuple
+from decimal import Decimal
+
+import msgpack
 
 from ..exceptions import ModelException
 from ..fields.geo_field import GeoField
-from ..redis_db import ENCODING, POPOTO_REDIS_DB
-
-from collections import namedtuple
+from ..redis_db import ENCODING
 
 EncoderDecoder = namedtuple("EncoderDecoder", "key, encoder, decoder")
 
 TYPE_ENCODER_DECODERS = {
     Decimal: EncoderDecoder(
         key='__Decimal__',
-        encoder=lambda obj: {'__Decimal__': True, 'as_str': str(obj)},
-        decoder=lambda obj: Decimal(obj['as_str'])
+        encoder=lambda obj: {'__Decimal__': True, 'as_encodable': str(obj)},
+        decoder=lambda obj: Decimal(obj['as_encodable'])
+    ),
+    tuple: EncoderDecoder(
+        key='__tuple__',
+        encoder=lambda obj: {'__tuple__': True, 'as_encodable': list(obj)},
+        decoder=lambda obj: tuple(obj['as_encodable'])
     ),
     set: EncoderDecoder(
         key='__set__',
-        encoder=lambda obj: {'__set__': True, 'as_str': str(list(obj))},
-        decoder=lambda obj: Decimal(obj['as_str'])
+        encoder=lambda obj: {'__set__': True, 'as_encodable': list(obj)},
+        decoder=lambda obj: set(obj['as_encodable'])
     ),
     datetime.datetime: EncoderDecoder(
         key='__datetime__',
-        encoder=lambda obj: {'__datetime__': True, 'as_str': obj.strftime("%Y%m%dT%H:%M:%S.%f")},
-        decoder=lambda obj: datetime.datetime.strptime(obj['as_str'], "%Y%m%dT%H:%M:%S.%f")
+        encoder=lambda obj: {'__datetime__': True, 'as_encodable': obj.strftime("%Y%m%dT%H:%M:%S.%f")},
+        decoder=lambda obj: datetime.datetime.strptime(obj['as_encodable'], "%Y%m%dT%H:%M:%S.%f")
     ),
     datetime.date: EncoderDecoder(
         key='__date__',
-        encoder=lambda obj: {'__date__': True, 'as_str': obj.strftime("%Y%m%d")},
-        decoder=lambda obj: datetime.datetime.strptime(obj['as_str'], "%Y%m%d").date()
+        encoder=lambda obj: {'__date__': True, 'as_encodable': obj.strftime("%Y%m%d")},
+        decoder=lambda obj: datetime.datetime.strptime(obj['as_encodable'], "%Y%m%d").date()
     ),
     datetime.time: EncoderDecoder(
         key='__time__',
-        encoder=lambda obj: {'__time__': True, 'as_str': obj.strftime("%H:%M:%S.%f")},
-        decoder=lambda obj: datetime.datetime.strptime(obj['as_str'], "%H:%M:%S.%f").time()
-    ),
-    GeoField.Coordinates: EncoderDecoder(
-        key='__Coordinates__',
-        encoder=lambda obj: {'__Coordinates__': True, 'latitude': obj.latitude, 'longitude': obj.longitude},
-        decoder=lambda obj: GeoField.Coordinates(obj['latitude'], obj['longitude'])
+        encoder=lambda obj: {'__time__': True, 'as_encodable': obj.strftime("%H:%M:%S.%f")},
+        decoder=lambda obj: datetime.datetime.strptime(obj['as_encodable'], "%H:%M:%S.%f").time()
     ),
 }
 TYPE_DECODER_KEYSTRINGS = [ed.key for ed in TYPE_ENCODER_DECODERS.values()]
 
 
 def decode_custom_types(obj):
-    if isinstance(obj, dict) and 'as_str' in obj:
+    if isinstance(obj, dict) and 'as_encodable' in obj:
         for encoder_decoder in TYPE_ENCODER_DECODERS.values():
             if encoder_decoder.key in obj:
                 return encoder_decoder.decoder(obj)
@@ -66,10 +65,12 @@ def encode_popoto_model_obj(obj: 'Model') -> dict:
         from ..fields.relationship import Relationship
         if value and isinstance(field, Relationship):
             if not isinstance(value, field.model):
-                raise ModelException(f"Relationship field requires {field.model} model instance. got {value} insteadd")
-            encoded_value = msgpack.packb(value.db_key)
-        elif field.type in TYPE_ENCODER_DECODERS.keys():
-            encoded_value = msgpack.packb(value, default=TYPE_ENCODER_DECODERS[field.type].encoder)
+                raise ModelException(f"Relationship field requires {field.model} model instance. got {value} instead")
+            encoded_value = msgpack.packb(value.db_key.redis_key)
+            # todo: refactor to store db_key list, not redis_key
+
+        elif value and field.type in TYPE_ENCODER_DECODERS.keys():
+            encoded_value = msgpack.packb(TYPE_ENCODER_DECODERS[field.type].encoder(value))
         else:
             encoded_value = msgpack.packb(value)
 
@@ -86,38 +87,3 @@ def decode_popoto_model_hashmap(model_class: 'Model', redis_hash: dict) -> 'Mode
         })
     else:
         return None
-
-
-class DB_key(list):
-    def __init__(self, *key_partials):
-        def flatten(yet_flat):
-            for item in yet_flat:
-                if isinstance(item, Iterable) and not isinstance(item, (str, bytes)):
-                    yield from flatten(item)
-                else:
-                    yield item
-
-        super().__init__(flatten(key_partials))
-
-    @classmethod
-    def clean(cls, value: str, ignore_colons: bool = False) -> str:
-        value = value.replace('/', '//')
-        for char in "'?*^[]-":
-            value = value.replace(char, f"/{char}")
-        if not ignore_colons:
-            value = value.replace(':', '_')
-        return value
-
-    def __str__(self):
-        return ":".join([self.clean(partial) for partial in self])
-
-    @property
-    def redis_key(self):
-        return str(self)
-
-    def exists(self):
-        return True if POPOTO_REDIS_DB.exists(self.redis_key) > 0 else False
-
-    def get_instance(self, model_class):
-        redis_hash = POPOTO_REDIS_DB.hgetall(self.redis_key)
-        return decode_popoto_model_hashmap(model_class, redis_hash)
