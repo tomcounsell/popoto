a63cfb3e9accaa1a4ad06b1b31b3075804cc2e4a
Tom Counsell <other@tomcounsell.com>
2022-05-18 16:18:53 +0700

    new tests for order_by and limits

--- tests/test_query_results.py
+++ tests/test_query_results.py
@@ -21,11 +21,17 @@ no_thing = ThingModel.create(int_key=0, str_key="0")
 r_thing = ThingModel.create(int_key=5, str_key="1", float_value=1.123, str_value="1.123")
 
 assert len(ThingModel.query.all()) == 4
+# test order_by
 assert ThingModel.query.all(order_by="float_value")[0] == no_thing
 assert ThingModel.query.all(order_by="str_key")[-1] == thing_2
 assert ThingModel.query.all(order_by="float_value", limit=3)[-1] == r_thing
-
 assert ThingModel.query.filter(str_key__startswith="1", order_by="int_key")[0] == thing_1
+# test limit
+assert len(ThingModel.query.all(limit=2)) == 2
+assert len(ThingModel.query.filter(str_key=1, limit=1)) == 1
+# test order_by with limit
+assert ThingModel.query.filter(str_key=1, order_by="int_key", limit=1)[0] == thing_1
+assert ThingModel.query.filter(str_key=1, order_by="-float_value", limit=1)[0] == r_thing
 
 for item in ThingModel.query.all():
     item.delete()
